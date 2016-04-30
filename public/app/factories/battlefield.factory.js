54722dde725eb11e566839517c19aaa352e84e0e
Ben <richterb3@gmail.com>
2016-04-29 19:02:08 -0700

    [Feature] player selects choice

--- public/app/factories/battlefield.factory.js
+++ public/app/factories/battlefield.factory.js
@@ -5,19 +5,32 @@ angular
 bfFactoryFunction.$inject = ['$http', 'socketFactory']; //injections go inside brackets
 
 function bfFactoryFunction($http, socketFactory) {
-
   //flags at top and then factory, then function declarations
-
+  var factoryChoice = []; //reset to choice upon function call
   var emit = socketFactory.emit;
   var on = socketFactory.on;
+  var getterObj = {choices: ['Rich', 'Bum', 'Tax', 'Cop', 'Jail']};
+
+  var factory = {
+    emit: emit,
+    setChoice: setChoice,
+    getter: getter
+  };
+
   on('gameReady', function(resp){
     console.log('Game ready: ', resp);
   });
 
-	var factory = {
-	};
-	return factory;
+  return factory;
 
-  //expose factory first; declare functions below
+  function setChoice(userChoice){
+    factoryChoice = userChoice;
+    emit('choices', {choice: factoryChoice});
+    console.log('this is the factoryChoice: ', factoryChoice);
+  }
+
+  function getter(name){
+    return getterObj[name];
+  }
 
 }
