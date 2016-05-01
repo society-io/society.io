9f5d405b0895605b4e1d707872d89b1188678554
Neil Agarwal <nagar001@ucr.edu>
2016-05-01 15:42:34 -0700

    [Feature] adds socket logic Server Side for privateGames

--- server/server.js
+++ server/server.js
@@ -46,19 +46,81 @@ io.on('connection', function(socket){
     console.log(usersSockets);
   });
 
-  socket.on('privateGame', function(data){
-   // Receive usernames from clients
-   // Use usernames to get sockets from users obj
-   // use sockets to instantiate a private game
+  // PRIVATE GAMES //
 
-  });
+  socket.on('newPrivateGame', storePrivateUser1(data));
+
+  socket.on('joinPrivateGame', storePrivateUser2(data), initiatePrivateGame(data));
+
+  // END PRIVATE GAMES
 
 });
 
-module.exports = {
-  users: usersSockets,
-  privateGames: privateGames
-};
+function storePrivateUser1(data){
+  // Check to see if joinCode Already Exists
+  if(!privateGames[data.joinCode]) { // if Not
+    // create a tuple for the users' sockets
+    var socketsForGame = [];
+    // Get the user's socket
+    socketsForGame[0] = retrieveSocket(data.name);
+    // store a privateGame ready to be initiated within the privateGames obj
+    privateGames[data.joinCode] = socketsForGame;
+    // Tell Client that the Game is waiting for player 2 to enter joinCode
+      // you could choose to show the user a "loading" view.
+    socket.emit('waitingForPlayer2', {
+      message: 'Great! Waiting on Player 2 to Enter your joinCode!'
+    });
+  } else { // if Yes
+    socket.emit('error', {
+      message: 'error! joinCode in use! Please enter a unique one.'
+    });
+  }
+}
+
+function storePrivateUser2(data, next){
+  // Crossreference joinCode with those in privateGames
+  for(var key in privateGames){
+    // if the request joinCode matches one within privateGames
+    if(privateGames[data.joinCode]) {
+      // retrieve this user's socket,
+        // and store within socketsForGame of that privateGame
+      privateGames[data.joinCode][1] = retrieveSocket(data.name);
+      next();
+    } else { // if Not
+      socket.emit('error', {
+        message: 'error! joinCode does not match our records. Please check with your friend & try again!'
+      });
+    }
+  }
+}
+
+function retrieveSocket(name){
+  // Crossreference name with those in users
+  for(var key in users){
+    // if the request name matches one in users
+    if(users[key] === name) {
+      // get that user's socket
+      return users[key];
+    } else { // if Not
+      // Tell Client that user isn't connected
+      socket.emit('error', {
+        message: 'error! User socket NOT connected!'
+      });
+    }
+  }
+}
+
+function initiatePrivateGame(data){
+  // Create a new privateGame using the right credentials
+  var privateGame = new Game(privateGames[data.joinCode]);
+  // Use the privateGame's initialize method
+  privateGame.init();
+  // Tell the Clients that a privateGame has been created & initiated
+  socket.emit('privateGameInitiated', {
+    message: '*privateGame Created & Initiated*',
+    players: privateGames[data.joinCode]
+  });
+}
 
 //***************************************************//
 
@@ -86,4 +148,3 @@ module.exports = {
       // calls game.evaluateWinner(),
         // the game method returns the winner,
         // sends the winner as a socket.emit message.
-
