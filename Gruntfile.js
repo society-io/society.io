59b4baab0e4643dbea403021e97b066154c4766d
Kan Adachi <Kadachi1023@gmail.com>
2016-05-02 19:46:58 -0700

    [Setup] Sass setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: ['public/app/**/*.js', 'server/**/*.js'],
-      tasks: ['jshint', 'concat'],
+      files: ['public/styles/**/*.scss', 'public/app/**/*.js', 'server/**/*.js'],
+      tasks: ['jshint', 'concat', 'sass'],
     },
     concat: {
       dist: {
@@ -25,12 +25,24 @@ module.exports = function(grunt) {
         ],
         dest: 'public/dist/built.js',
       }
+    },
+    sass: {
+      options: {
+        sourceMap: true,
+        sourceComments: false
+      },
+      dist: {
+        files: {
+          'public/dist/style.css': 'public/styles/style.scss'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.registerTask('default', ['jshint', 'concat']);
 };
