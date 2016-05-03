ab8c5af7288dacc4b1998aa5f1753b950023084b
Neil Agarwal <nagar001@ucr.edu>
2016-05-03 11:43:06 -0700

    [Feature] server sends clients player picture for private games

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -3,12 +3,12 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var facebookAuth = require('./config').facebookAuth;
 var User = require('../db/userModel');
 var userFBID = null;
+var userPhoto = null;
 
 // used to serialize the user for the session
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
-
 // used to deserialize the user
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
@@ -21,9 +21,8 @@ passport.use(
     clientID: facebookAuth.clientID,
     clientSecret: facebookAuth.clientSecret,
     callbackURL: facebookAuth.callbackURL,
-    profileFields: ["id", "displayName", "emails", "picture"]
+    profileFields: ["id", "displayName", "emails", "photos"]
   }, usertoDB));
-
 
 function usertoDB(accessToken, refreshToken, profile, done) {
   process.nextTick(function(){
@@ -32,6 +31,7 @@ function usertoDB(accessToken, refreshToken, profile, done) {
         return done(err);
       }
       if(user) {
+        console.log('User in DB: ', user);
         return done(null, user);
       } else {
        user = new User({
@@ -39,16 +39,19 @@ function usertoDB(accessToken, refreshToken, profile, done) {
         name: profile.displayName,
         email: profile.emails[0].value,
         photo: profile.photos[0].value,
+        accessToken: accessToken,
         mmr: 1600,
         wins: 0,
-	      losses:0
-	      });
+        losses:0
+        });
         user.save(function(err){
           if(err) {
             throw err;
           }
           // Store this user's fbid to associate it with this user's socket
+          console.log('user.picture: ', user.photo);
           userFBID = user.fbid;
+          userPhoto = user.photo;
           return done(null, user);
         });
       }
@@ -56,4 +59,4 @@ function usertoDB(accessToken, refreshToken, profile, done) {
   });
 }
 
-module.exports = {passport: passport, userFBID: userFBID};
+module.exports = {passport: passport, userFBID: userFBID, userPhoto: userPhoto};
