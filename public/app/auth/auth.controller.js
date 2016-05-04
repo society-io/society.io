e9ec90b142f124fde9f397abaaa4e2fd1a1c1023
Ben <richterb3@gmail.com>
2016-05-03 18:51:04 -0700

    [Feature] Signup/signin page redone without fb

--- public/app/auth/auth.controller.js
+++ public/app/auth/auth.controller.js
@@ -9,6 +9,16 @@ AuthController.$inject = [];
 function AuthController() {
 	/* jshint validthis: true */
 	var vm = this;
+	this.signup = false;
+	this.signin = false;
+
+
+
+	vm.signup = function(obj){
+		Auth.signup(obj).then(function(resp) {
+			console.log(resp);
+		})
+	}
 
 }
 })();
---
+++ public/app/auth/auth.factory.js
@@ -0,0 +1,56 @@
+(function (){
+  angular
+    .module('app')
+    .factory('authFactory', authFactory);
+    
+    authFactory.$inject = ['$http', '$window', '$state', '$rootScope'];
+    
+    function authFactory($http, $window, $state, $rootScope) {
+
+    	return {
+    		signup: signup,
+    		signin: signin
+    	}
+
+
+
+
+
+    	function signup(userObj){
+    		var request = {
+    			method: 'POST',
+    			url: '/signup',
+    			data: userObj
+    		}
+
+    		return $http(request)
+    			.then(success, error);
+
+    		function success(resp){
+    			console.log('successful signup: ', resp);
+    		}
+    		function error(err){
+    			return console.error(err);
+    		}
+    	}
+
+    	function signin(userObj){
+    		var request = {
+    			method: 'POST',
+    			url: '/signin',
+    			data: userObj
+    		}
+
+    		return $http(request)
+    			.then(success, error);
+
+    		function success(resp){
+    			console.log('successful signup: ', resp);
+    		}
+    		function error(err){
+    			return console.error(err);
+    		}
+    	}
+
+    }
+})();
