ee223dd2e916ee86ceea95be92b3de5c0e589a71
Kan Adachi <Kadachi1023@gmail.com>
2016-05-05 16:37:15 -0700

    [Feature] Fadein in-game waiting overlay

--- server/game/listeners.js
+++ server/game/listeners.js
@@ -6,25 +6,18 @@ var onDisconnect = function(player) {
 
 var onChoice = function(data) {
   // this = player obj
-  console.log('inside of onChoice ====== ');
-  console.log('inside of onChoice. data.choice = ', data.choice);
-  console.log('========================= ');
   this.updateChoice(data.choice);
 };
 
 var onNoChoice = function() {
   // this = player obj
-  console.log('inside of onNoChoice =====');
-  console.log('player = ', player);
   this.updateChoice('noChoice');
 };
 
 var onClientGameReady = function(player) {
-  console.log('inside of onClientGameReady');
   // this = player obj
   this[player].gameReady = true;
   if (this.player1.gameReady && this.player2.gameReady) {
-    console.log('emitting newRound');
     this.newRound();
   }
 };
