55b5f38776f8d5d70750047ece282261c8b0c19a
Patrice Sandhu <pmsandhu@ucla.edu>
2016-05-04 23:39:17 -0700

    [Feature] reimplemented and refactored server game queue

--- server/lobby/queue.js
+++ server/lobby/queue.js
@@ -1,40 +1,51 @@
-var io = require('../common').io;
+var io = require('../common');
+var io = require('socket.io');
+var game = require('../game/config');
 
-var queue = {
-  storage: [],
-  insert: function(socket) {
-    this.storage.push(socket);
-    console.log('QUEUE', this.storage.length);
-    socket.on('disconnect', function(socket) {
-      var idx = Queue.storage.indexOf(socket);
-      Queue.storage.splice(idx, 1);
-      socket.emit('added to queue');
-      console.log(Queue.storage);
-    });
-  },
-  remove: function () {
-    var socket1 = this.storage.shift();
-    var socket2 = this.storage.shift();
-    socket.emit('match ready');
-    return {player1: socket1, player2: socket2};
-  }
-};
+var queue = [];
+	
+function decideQueue(socket) {
+	if(queue.length === 0) {
+		addToQueue(socket);
+	} else {
+		socket.emit('added to queue');
+		queueMatch(socket);
+	}
+}
 
-io.on('queue', function(socket) {
-  console.log('heard queue event');
-  console.log(queue);
-  // Put socket into queue
-  if(queue.storage.length < 2) {
-    queue.insert(socket);
-  }
-  // Instantiate game if more than 2 in queue
-  if (queue.storage.length >= 2) {
-    var playerSockets = queue.remove();
-    var game = new Game(playerSockets);
-    game.init();
-  }
-});
+function addToQueue(socket) {
+	queue.push(socket);
+	console.log('ADDED TO QUEUE:', queue);
+	socket.emit('added to queue');
+}
+
+function queueMatch(socket) {
+	if (queue.length>=2) {
+		var match = queue.pop();
+		console.log('QUEUE MATCH: player1 = ', match,' player2 = ', socket.id);
+		socket.emit('match ready');
+		gameInit(activeSockets[match.id], socket);
+	}
+}
+
+function gameInit(socket1, socket2){
+	var game = new Game(socket1, socket2);
+	game.init();
+	socket.emit('game ready');
+}
+
+function disconnected(socket) {
+	var index = queue.indexOf(socket.id);
+	queue.splice(index, 1);
+	console.log('DISCONNECTED FROM QUEUE:', queue);
+}
+
+
 
 module.exports = {
-  queue: queue
+	decideQueue: decideQueue, 
+	addToQueue: addToQueue, 
+	queueMatch: queueMatch, 
+	gameInit: gameInit,
+	disconnected: disconnected
 };
