da02c3b4d4ae0ef9af597f24bed2d03f1a00a4eb
Neil Agarwal <nagar001@ucr.edu>
2016-05-05 16:10:11 -0700

    [Feature] loading state created

--- public/app/ui-router/app.config.js
+++ public/app/ui-router/app.config.js
@@ -14,18 +14,24 @@ function config($stateProvider, $urlRouterProvider) {
 			controller: 'AuthController',
 			controllerAs: 'Auth'
 		})
-		.state('waiting', {
-			url: '/waiting',
-			templateUrl: '../app/waiting/waiting.html',
-			controller: 'WaitingController',
-			controllerAs: 'Waiting'
-		})
 	  .state('lobby', {
 	  	url: '/lobby',
 	  	templateUrl: '../app/lobby/lobby.html',
 	  	controller: 'LobbyController',
 	  	controllerAs: 'Lobby'
 	  })
+	  .state('loading', {
+	  	url: '/loading',
+	  	templateUrl: '../app/loading/loading.html',
+	  	controller: 'LoadingController',
+	  	controllerAs: 'Loading'
+	  })
+		.state('waiting', {
+			url: '/waiting',
+			templateUrl: '../app/waiting/waiting.html',
+			controller: 'WaitingController',
+			controllerAs: 'Waiting'
+		})
 	  .state('battlefield', {
 	  	url: '/battlefield',
 	  	templateUrl: '../app/battlefield/battlefield.html',
