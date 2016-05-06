177dba8fac6f6404e73ffbb327622d985b8a15ae
Kan Adachi <Kadachi1023@gmail.com>
2016-05-06 10:42:23 -0700

    [Setup] Disabled z.js testing battlefield file

--- server/z.js
+++ server/z.js
@@ -5,20 +5,20 @@ var SocketAPI = require('./socket/socketAPI').SocketAPI;
 var queue = [];
 var sockets = {};
 
-io.on('connection', function(socket) {
-  socket.on('newGame', function() {
-    console.log('hi');
-    sockets[socket.id] = queue.length;
-    queue.push(socket);
-    if (queue.length === 2) {
-      console.log('instantiating game!');
-      var socket1 = new SocketAPI(queue.pop());
-      var socket2 = new SocketAPI(queue.pop());
-      new Game(socket1, socket2).init();
-    }
-  });
-});
+// io.on('connection', function(socket) {
+//   socket.on('newGame', function() {
+//     console.log('hi');
+//     sockets[socket.id] = queue.length;
+//     queue.push(socket);
+//     if (queue.length === 2) {
+//       console.log('instantiating game!');
+//       var socket1 = new SocketAPI(queue.pop());
+//       var socket2 = new SocketAPI(queue.pop());
+//       new Game(socket1, socket2).init();
+//     }
+//   });
+// });
 
-io.on('disconnect', function(socket) {
-  queue.splice(sockets[socket.id], 1);
-});
+// io.on('disconnect', function(socket) {
+//   queue.splice(sockets[socket.id], 1);
+// });
