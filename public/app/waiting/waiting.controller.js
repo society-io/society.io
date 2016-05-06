9fd3c3099aa695eedbd4f621b899ab96956dd3ca
Austin Yearly Kim <austinyearlykim@gmail.com>
2016-05-06 11:47:51 -0700

    [Feature] Controller getters for players data

--- public/app/waiting/waiting.controller.js
+++ public/app/waiting/waiting.controller.js
@@ -11,10 +11,18 @@ angular
 
     var vm = this;
 
-    vm.playerOne = function(){
+    vm.playerOneName = function() {
     	return waitingFactory.get('player1Name');
     };
-
+    vm.playerOneMMR = function() {
+    	return waitingFactory.get('player1MMR');
+    };
+    vm.playerTwoName = function() {
+    	return waitingFactory.get('player1Name');
+    };
+    vm.playerTwoMMR = function() {
+    	return waitingFactory.get('player2MMR');
+    };
   }
 
 })();
