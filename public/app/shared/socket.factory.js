8ce31fdeca2f0ca282d6c08a1831a6ff8af3c97a
Kan Adachi <Kadachi1023@gmail.com>
2016-05-09 15:12:28 -0700

    [Bugfix] game-end update mmr function commented out

--- public/app/shared/socket.factory.js
+++ public/app/shared/socket.factory.js
@@ -2,50 +2,57 @@ angular
   .module('app')
   .factory('socketFactory', socketFactory);
 
-	socketFactory.$inject = ['$rootScope', 'authFactory'];
+  socketFactory.$inject = ['$rootScope', 'authFactory'];
 
-	function socketFactory($rootScope, authFactory) {
+  function socketFactory($rootScope, authFactory) {
 
-		var socket;
+    var socket;
 
-		return {
-		  connectSocket:connectSocket,
-		  on: on,
-		  emit: emit
-		};
+    return {
+      connectSocket:connectSocket,
+      on: on,
+      emit: emit,
+      isConnected: isConnected
+    };
 
-	  function connectSocket() {
-	    socket = io.connect();
-	    socket.emit('init', authFactory.attachToken({}));
-		}
+    function isConnected() {
+      return !!socket;
+    }
 
-		/**
-		 *	Socket Events
-		 *
-		 *	Used to wrap the socket's native 'on' and 'emit' functions
-		 *	packet envelope that we specify
-		 */
-	  function on(eventName, callback) {
-	    socket.on(eventName, function() {
-	      var args = arguments;
-	      $rootScope.$apply(function() {
-	        callback.apply(socket, args);
-	      });
-	    });
-	  }
+    function connectSocket() {
+      if (!socket) {
+        socket = io.connect();
+        socket.emit('init', authFactory.attachToken({}));
+      }
+    }
 
-	  function emit(eventName, data, callback, auth) {
-	  	if (auth) {
-	  		data = data || {};
-	  		data = authFactory.attachToken(data);
-	  	}
-	    socket.emit(eventName, data, function() {
-	      var args = arguments;
-	      $rootScope.$apply(function() {
-	        if(callback) {
-	          callback.apply(socket, args);
-	        }
-	      });
-	    });
-	  }
-	}
+    /**
+     *  Socket Events
+     *
+     *  Used to wrap the socket's native 'on' and 'emit' functions
+     *  packet envelope that we specify
+     */
+    function on(eventName, callback) {
+      socket.on(eventName, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          callback.apply(socket, args);
+        });
+      });
+    }
+
+    function emit(eventName, data, callback, auth) {
+      if (auth) {
+        data = data || {};
+        data = authFactory.attachToken(data);
+      }
+      socket.emit(eventName, data, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          if(callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  }
