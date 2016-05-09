806f5733fedc38f21296e8b9a97edf98b61a8f55
Austin Yearly Kim <austinyearlykim@gmail.com>
2016-05-09 16:42:49 -0700

    [Feature] GET route for leaderboard successfully returns correct res object

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -4,11 +4,9 @@ var db = require('../db/userModel.js');
 router.get('/', function(req, res){
   db.find({}, function(err, users) {
   	var userMap = {};
-
   	users.forEach(function(user) {
-  		userMap[user._id] = user;
+  		userMap[user._id] = {username: user.username, wins: user.wins, losses: user.losses, mmr: user.mmr};
   	});
-
   	res.send(userMap);
   });
 });
