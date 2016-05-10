7ca278f92bbec53ef5789e99dc7e107a6338639e
Austin Yearly Kim <austinyearlykim@gmail.com>
2016-05-09 17:30:29 -0700

    [Feature] leaderboard imported into state object

--- public/app/lobby/stats.factory.js
+++ public/app/lobby/stats.factory.js
@@ -6,14 +6,23 @@ angular
   statsFactory.$inject = ['$http'];
 
   function statsFactory($http) {
-  	
-    var state = {};
 
-  	return {getUsers: getUsers};
+    var state = {leaderboard: null};
+    
+    getUsersFromDB().then(function(users) {
+      	state[leaderboard] = users;
+    });
 
-    function getUsers ($http) {
+  	return {get: get};
+
+    function getUsersFromDB($http) {
       return $http.get('/leaderboard');
     }
+
+    function get(key) {
+    	return state[key];
+    }
+
   }
 
 })();
