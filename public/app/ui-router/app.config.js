8f8c60d1cea9af86aee132ce2c332ef1e48eedb7
Kan Adachi <Kadachi1023@gmail.com>
2016-05-17 21:33:41 -0700

    [Feature] chat styled

--- public/app/ui-router/app.config.js
+++ public/app/ui-router/app.config.js
@@ -5,56 +5,46 @@ angular
 
 function config($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.otherwise('/auth');
+  $urlRouterProvider.otherwise('/auth');
 
-	$stateProvider
-		.state('auth', {
-			url: '/auth',
-			templateUrl: '../app/auth/auth.html',
-			controller: 'AuthController',
-			controllerAs: 'Auth',
-			data: {
-				bodyClasses: 'auth'
-			}
-		})
-	  .state('lobby', {
-	  	url: '/lobby',
-	  	templateUrl: '../app/lobby/lobby.html',
-	  	controller: 'LobbyController',
-	  	controllerAs: 'Lobby',
-	  	data: {
-	  		bodyClasses : 'lobby',
-	  		auth: true
-	  	}
-	  })
-	  .state('chat', {
-	  	url: '/chat',
-	  	templateUrl: '../app/chat/chat.html',
-	  	controller: 'ChatController',
-	  	controllerAs: 'Chat',
-	  	data: {
-	  		bodyClasses: 'lobby',
-	  		auth: true
-	  	}
-	  })
-		.state('waiting', {
-			url: '/waiting',
-			templateUrl: '../app/waiting/waiting.html',
-			controller: 'WaitingController',
-			controllerAs: 'Waiting',
-			data: {
-				bodyClasses: 'waiting',
-				auth: true
-			}
-		})
-	  .state('battlefield', {
-	  	url: '/battlefield',
-	  	templateUrl: '../app/battlefield/battlefield.html',
-	  	controller: 'BattlefieldController',
-	  	controllerAs: 'Bf',
-	  	data: {
-	  		bodyClasses: 'battlefield',
-	  		auth: true
-	  	}
-	  });
+  $stateProvider
+    .state('auth', {
+      url: '/auth',
+      templateUrl: '../app/auth/auth.html',
+      controller: 'AuthController',
+      controllerAs: 'Auth',
+      data: {
+        bodyClasses: 'auth'
+      }
+    })
+    .state('lobby', {
+      url: '/lobby',
+      templateUrl: '../app/lobby/lobby.html',
+      controller: 'LobbyController',
+      controllerAs: 'Lobby',
+      data: {
+        bodyClasses : 'lobby',
+        auth: true
+      }
+    })
+    .state('waiting', {
+      url: '/waiting',
+      templateUrl: '../app/waiting/waiting.html',
+      controller: 'WaitingController',
+      controllerAs: 'Waiting',
+      data: {
+        bodyClasses: 'waiting',
+        auth: true
+      }
+    })
+    .state('battlefield', {
+      url: '/battlefield',
+      templateUrl: '../app/battlefield/battlefield.html',
+      controller: 'BattlefieldController',
+      controllerAs: 'Bf',
+      data: {
+        bodyClasses: 'battlefield',
+        auth: true
+      }
+    });
 }
